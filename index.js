a3fd1a6a4e7830b92a3c9b6936500fa9d91f0c21
romyjkk <romyjongkees02@gmail.com>
2022-03-07 11:57:05 +0100

    updates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const PORT = 3000
+
 express()
     .use('/static', express.static('static'))
 
@@ -7,7 +9,7 @@ express()
     .get('/about', onabout)
     .get('/match', onmatch)
 
-    .listen(8000)
+    .listen(3000)
 
 function onhome(req, res) {
     res.send('<h1>Hello World!</h1>')
@@ -20,3 +22,20 @@ function onabout(req, res) {
 function onmatch(req, res) {
     res.send('<h1>Start swiping!</h1>')
 }
+
+/* handlebars installation */
+
+import express from 'express'
+import { engine } from 'express-handlebars'
+
+const app = express()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000)
